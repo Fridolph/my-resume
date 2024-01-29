9761b727831c73ce01c1c2198d84cd965d51ab8c
fridolph <249121486@qq.com>
2024-01-29 12:34:30 +0800

    build: :building_construction: 添加公共资源，准备项目开发

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{ts,tsx,vue}',
-  ],
+  content: ['./index.html', './src/**/*.{ts,tsx,vue}'],
   theme: {
     container: {
       // center: true,
-      padding: '1rem'
+      padding: '1rem',
     },
     screens: {
       sm: '640px',
       md: '1024px',
-      lg: '1400px',      
+      lg: '1400px',
       xl: '1920px',
-      xxl: '2560px'
+      xxl: '2560px',
     },
-    extend: {},
+    extend: {
+      animation: {
+        'singerShake': 'singerShake linear infinite',
+      },
+      keyframes: {
+        circleLoadRight: {
+          '0%': { transform: 'rotate(-135deg)' },
+          '50%, 100%': { transform: 'rotate(45deg)' },
+        },
+        circleLoadLeft: {
+          '50%': { transform: 'rotate(-135deg)' },
+          '100%': { transform: 'rotate(45deg)' },
+        },
+        singerShake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '10%, 30%, 50%, 70%, 90%': { transform: `translateX(-2px) scale(1.05)` },
+          '20%, 40%, 60%, 80%': { transform: `translateY(2px) scale(1)` },
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
-
