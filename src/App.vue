14d37d22077c956a8c48cdf7d767b1fece8d71d8
fridolph <249121486@qq.com>
2024-01-29 12:35:32 +0800

    build: :building_construction: 添加iconfont、公共css 基础样式处理

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
-  <!-- <img
-    class="aspect-video"
-    src="/vite.svg" /> -->
-  <!-- <img
-    class="aspect-square"
-    src="./assets/vue.svg" /> -->
-  <div class="container">
-    <div class="flex items-center">
-      <div class="">
-        <img
-          class="aspect-square"
-          src="./assets/vue.svg" />
-      </div>
-      <div class="flex flex-col ml-2">
-        <strong>Fridolph</strong>
-        <span>前端工程师</span>
-      </div>
-    </div>
+  <div class="w-full min-h-screen md:flex md:justify-center md:align-middle p-0  bg-[#778899] md:p-5 lg:p-10 xl:px-[5%] xl:py-20">
+    <section
+      class="sm:w-full md:w-full max-w-[1600px] flex flex-col md:flex-row justify-center align-middle text-color">
+      <Aside>
+        <Baseinfo />
+        <Contact />
+        <Hobby />
+        <Wechat />
+      </Aside>
+      <Main> 
+        <Education />
+        <Skill />
+        <Experience />
+      </Main>
+    </section>
   </div>
 </template>
 <script setup lang="ts">
-// import { Button, Message } from 'fri-element-plus'
+import Aside from './components/Layouts/Aside.vue'
+import Main from './components/Layouts/Main.vue'
+import Baseinfo from './views/Baseinfo/Baseinfo.vue'
+import Contact from './views/Contact/Contact.vue'
+import Hobby from './views/Hobby/Hobby.vue'
+import Wechat from './views/Wechat/Wechat.vue'
+import Education from './views/Education/Education.vue'
+import Skill from './views/Skill/Skill.vue'
+import Experience from './views/Experience/Experience.vue'
 </script>
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
