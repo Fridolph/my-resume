7cdb051d832486cb18d50056510b9f7d0e85e09d
fridolph <249121486@qq.com>
2024-02-08 02:50:59 +0800

    build: :green_heart: vite.config.js 修改

--- vite.config.ts
+++ vite.config.ts
@@ -23,8 +23,7 @@ export default defineConfig({
       customSplitting: {
         // `vue` 会被打包到一个名为`vue-vendor`的 chunk 里面(包括它们的一些依赖，如 object-assign)
         'vue-vendor': [/vue/],
-        'tailwind-vendor': [/tailwindcss/],
-        'fri-vendor': [/fri-element-plus/],
+        'tailwind-vendor': [/tailwindcss/],        
         // 源码中目录的代码都会打包进 [name] 这个 chunk 中
         'styles': [/src\/styles/],
         'locales': [/src\/locales/]
