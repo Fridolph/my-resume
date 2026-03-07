88930c56476665425f7cd426668990535826d8a8
fridolph <249121486@qq.com>
2026-03-07 20:25:55 +0800

    fix(api-server): register site settings module

--- apps/api-server/src/app.module.ts
+++ apps/api-server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from './modules/database/database.module.js'
 import { HealthModule } from './modules/health/health.module.js'
+import { SiteSettingsModule } from './modules/site-settings/site-settings.module.js'
 
 @Module({
-  imports: [DatabaseModule, HealthModule]
+  imports: [DatabaseModule, HealthModule, SiteSettingsModule]
 })
 export class AppModule {}
